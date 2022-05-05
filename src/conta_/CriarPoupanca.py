b42fa31cfbe91e03b321e9d9424d194b02b6d5b1
aly10joj <maria.alyce1011@hotmail.com>
2022-05-05 11:25:55 -0300

    POUP CONTA CRIAR

--- src/conta_/CriarPoupanca.py
+++ src/conta_/CriarPoupanca.py
@@ -1,13 +1,13 @@
 from Poupanca import ContaPoupanca
 
-
 class CriarPoupanca:
     if __name__ == '__main__':
-        poupanca = ContaPoupanca("21.342-7")
-        poupanca.creditar(500.87)
-        poupanca.debitar(45.00)
+        conta = ContaPoupanca("21.342-7")
+        conta = ContaPoupanca(conta)
+        conta.creditar(500.87)
+        conta.debitar(45.00)
 
-        print(poupanca.get_saldo())
+        print(conta.get_saldo())
 
-        poupanca.render_juros(0.01)
-        print(poupanca.get_saldo())
+        conta.render_juros(0.01)
+        print(conta.get_saldo())
