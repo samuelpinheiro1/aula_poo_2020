d6919a8f719587a70cecffb4966ab40a96ad79b8
aly10joj <maria.alyce1011@hotmail.com>
2022-05-10 10:59:42 -0300

    CONTA ESPECIAL

---
+++ src/conta_/ContaEspecial.py
@@ -0,0 +1,17 @@
+from src.conta_.conta import Conta
+
+
+class ContaEspecial(Conta):
+    def __init__(self, numero):
+
+        def __init__(self, numero):
+            super().__init__(numero)
+            self.__bonus = 0
+
+    def renderBonus(self):
+        super().creditar(self.__bonus)
+        self.__bonus = 0
+
+    def creditar(self, valor):
+        self.__bonus = self.__bonus + (valor * 0.01)
+        super().creditar(valor)
